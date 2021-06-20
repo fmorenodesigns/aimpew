b978e95ca32136582ef13e71618c6002d287abea
Francisco Moreno <50809863+fmoredesigns@users.noreply.github.com>
2021-06-20 10:36:40 +0200

    Make targets blue when hit

--- src/pages/Game/components/Target/index.js
+++ src/pages/Game/components/Target/index.js
@@ -1,13 +1,20 @@
 import "./styles.scss";
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function Target({ size, left, top, onHit }) {
+  const [hit, setHit] = useState(false);
+
   return (
     <div
-      className="target"
+      className={`target ${hit ? "hit" : ""}`}
       style={{ width: size, height: size, left, top }}
-      onClick={onHit}
+      onClick={() => {
+        setHit(true);
+        setTimeout(() => {
+          onHit();
+        }, [200]);
+      }}
     >
       <div className="white">
         <div className="red">
